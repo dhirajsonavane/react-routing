c399d47e3c6cf1ca0f75c122e80e050b5ffd475b
Sonavane, Dhiraj <Dhiraj.Sonavane@tabcorp.com.au>
2020-04-15 19:00:54 +1000

    Updates

---
+++ precache-manifest.56bcdca0632595eb817417214cc6460e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1601eabb20f36ca61ac7e85d6feefb00",
+    "url": "/react-routing/index.html"
+  },
+  {
+    "revision": "4600a48081c2df810072",
+    "url": "/react-routing/static/css/main.686ddf0b.chunk.css"
+  },
+  {
+    "revision": "5a99963557c571a91188",
+    "url": "/react-routing/static/js/2.72b1e99e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-routing/static/js/2.72b1e99e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4600a48081c2df810072",
+    "url": "/react-routing/static/js/main.815a7b37.chunk.js"
+  },
+  {
+    "revision": "f655a6f1891b8e20f716",
+    "url": "/react-routing/static/js/runtime-main.b1415307.js"
+  }
+]);
